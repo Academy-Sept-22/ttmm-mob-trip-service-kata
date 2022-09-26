f84664d6e0935e01068636c468fa71b248d9f9c9
Meryem Sel <mzbasel637936@gmail.com>
2022-09-26 14:17:43 +0100

    Trip service has been partly refactored

--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -2,40 +2,47 @@ package org.craftedsw.tripservicekata.trip;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
+import org.craftedsw.tripservicekata.user.UserSession;
 
 public class TripService {
 
-	private final UserSessionProvider userSessionProvider;
-	private final TripsDAOProvider tripsDAOProvider;
+    private final UserSessionProvider userSessionProvider;
+    private final TripsDAOProvider tripsDAOProvider;
 
-	public TripService(TripsDAOProvider tripsDAOProvider, UserSessionProvider userSessionProvider) {
-		this.userSessionProvider = userSessionProvider;
-		this.tripsDAOProvider = tripsDAOProvider;
-	}
+    public TripService(TripsDAOProvider tripsDAOProvider, UserSessionProvider userSessionProvider) {
+        this.userSessionProvider = userSessionProvider;
+        this.tripsDAOProvider = tripsDAOProvider;
+    }
 
 
-	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
-		List<Trip> tripList = new ArrayList<Trip>();
-		User loggedUser = userSessionProvider.getUserSessionInstance().getLoggedUser();
-		boolean isFriend = false;
-		if (loggedUser != null) {
-			for (User friend : user.getFriends()) {
-				if (friend.equals(loggedUser)) {
-					isFriend = true;
-					break;
-				}
-			}
-			if (isFriend) {
-				tripList = tripsDAOProvider.findTripsByUser(user);
-			}
-			return tripList;
-		} else {
-			throw new UserNotLoggedInException();
-		}
-	}
+    public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
+        List<Trip> tripList = new ArrayList<Trip>();
+        if (isFriend(user, checkUserLoggedIn())) {
+            tripList = tripsDAOProvider.findTripsByUser(user);
+        }
+        return tripList;
+    }
+
+    private User checkUserLoggedIn() {
+        Optional<User> userOptional = Optional.ofNullable(userSessionProvider.getUserSessionInstance().getLoggedUser());
+        if (userOptional.isEmpty()) {
+            throw new UserNotLoggedInException();
+        }
+        return userOptional.get();
+    }
+
+    private boolean isFriend(User user, User loggedUser) {
+        for (User friend : user.getFriends()) {
+            if (friend.equals(loggedUser)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
 
 }
