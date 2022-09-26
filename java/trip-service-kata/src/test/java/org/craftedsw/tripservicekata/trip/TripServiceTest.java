2168a1007c6bc2122344ca9c79b2077b7dd6a322
MattW <mattnwarnock@yahoo.co.uk>
2022-09-26 09:52:04 +0100

    Idea for first unit test

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,15 @@
 package org.craftedsw.tripservicekata.trip;
 
+import org.junit.jupiter.api.Test;
+
 public class TripServiceTest {
+    // happy path: valid user is logged in
+    // mock logged user
+    // mock tripDAO
+    // returns triplist if logged user is a friend of the given user
+
+    @Test
+    void returns_users_trip_list_if_logged_user_is_a_friend_of_the_given_user() {
+    }
 	
 }
